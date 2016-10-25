868d5380316e6783ff8fb31587c15f42486ebca9
Allan L. R. Hansen <allan.lrh@gmail.com>
2016-10-25 15:50:18 +0200

    Added files in speclib

---
+++ speclib/loaders.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+import os
+import sys
+import json
+import codecs
+from pudb import set_trace
+
+
+def loadAndersJson(filepath):
+    if not os.path.isfile(filepath):
+        raise FileNotFoundError("The file {} doesn't seem to exist".format(filepath))
+
+    def cleanLine(ln):
+        ruleTup = (("u'", "'"),
+                   ('"', "\\"),
+                   ("'", '"'))
+        for rule in ruleTup:
+            ln = ln.replace(*rule)
+        return ln
+
+    def decodeLine(byteLine):
+        lst = byteLine.split(b"\\n")
+        toConcat = [codecs.escape_decode(line)[0].decode("iso-8859-15") for line in lst]
+        return "\\n".join([el for el in toConcat if el])
+
+    with open(filepath, "br") as fid:
+        for line in fid:
+            lineDecoded = decodeLine(line)
+            # yield ujson.loads(cleanLine(lineDecoded))
+            try:
+                cleaned = cleanLine(lineDecoded)
+                yield json.loads(cleaned)
+            except json.decoder.JSONDecodeError as err:
+                print(err, file=sys.stderr)
+                print(repr(cleanLine(lineDecoded)), file=sys.stderr)
+                set_trace()
+
+
+def loadUser(user, dataPath='/lscr_paper/allan/data/Telefon/userfiles'):
+    userPath = os.path.join(dataPath, user)
+    datafileList = os.listdir(userPath)
+    userDict = dict()
+    for filename in datafileList:
+        print(filename)
+        dataType = filename.split('_')[0]
+        datafilePath = os.path.join(userPath, filename)
+        print(datafilePath)
+        userDict[dataType] = list(loadAndersJson(datafilePath))
+    return userDict
+
+
+if __name__ == '__main__':
+    datapath = '/lscr_paper/allan/data/Telefon/userfiles'
+    userList = [el for el in os.listdir(datapath) if os.path.isdir(os.path.join(datapath, el))]
+    print(userList[0])
+    data = loadUser(userList[0])
---
+++ speclib/pushbulletNotifier.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+from pushbullet import Pushbullet
+import datetime
+import inspect
+
+
+phoneName = "OnePlus ONE A2003"
+
+
+class JobNotification(object):
+    """Class to send push notification when a job finishes, either because of an error, or
+    because it's completed.
+    """
+
+    def __init__(self, devices="all"):
+        super(JobNotification, self).__init__()
+        self.startTime = datetime.datetime.now()
+        self.timeFormatString = r'%d/%m/%Y %H:%M:%S'
+        if devices == "all":
+            self.pb = Pushbullet("o.B8s0B7VUNVgrzU4fNpvTjan4VKPO6qhJ")
+        elif devices == "phone":
+            for dv in Pushbullet("o.B8s0B7VUNVgrzU4fNpvTjan4VKPO6qhJ").devices:
+                if dv.nickname == phoneName:
+                    self.pb = dv
+
+    def pretty_time_delta(self, td):
+        seconds = int(td.total_seconds())
+        days, seconds = divmod(seconds, 86400)
+        hours, seconds = divmod(seconds, 3600)
+        minutes, seconds = divmod(seconds, 60)
+        if days > 0:
+            return '%dd%dh%dm%ds' % (days, hours, minutes, seconds)
+        elif hours > 0:
+            return '%dh%dm%ds' % (hours, minutes, seconds)
+        elif minutes > 0:
+            return '%dm%ds' % (minutes, seconds)
+        else:
+            return '%ds' % (seconds,)
+
+    def send(self, exception=None):
+        self.endTime = datetime.datetime.now()
+        callerFilename = inspect.stack()[1].filename
+        if exception:
+            title = "Error! {callerFilename} have crashed!".format(callerFilename=callerFilename)
+        else:
+            title = "{callerFilename} is done".format(callerFilename=callerFilename)
+        msgLst = ["Runtime: {timeDelta}", "Started: {timeStarted}", "Finished: {timeEnded}"]
+        if exception:
+            msgLst = ["Error!:  " + str(exception)] + msgLst
+        msg = "\n".join(msgLst).format(
+            callerFilename=callerFilename,
+            timeDelta=self.pretty_time_delta(self.endTime - self.startTime),
+            timeStarted=self.startTime.strftime(self.timeFormatString),
+            timeEnded=self.endTime.strftime(self.timeFormatString))
+        self.pb.push_note(title, msg)
+
+
+if __name__ == '__main__':
+    jn = JobNotification(devices="phone")
+    try:
+        1/0
+    except Exception as e:
+        jn.send(e)
