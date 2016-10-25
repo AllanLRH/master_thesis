3e7782357bf098d0356df79e1b4b15f2805997ff
Allan L. R. Hansen <allan.lrh@gmail.com>
2016-10-25 15:55:24 +0200

    Added ssh Jupyter notebook realted scripts

---
+++ sshRelated/killNotebook.py
@@ -0,0 +1,40 @@
+#!/lscr_paper/allan/miniconda3/envs/msth/bin/python
+
+import psutil
+import sys
+
+
+username = psutil.os.environ["USER"]
+
+
+def isInputPort(inp):
+    return inp.strip().isdigit()
+
+
+def getNotebooks():
+    return [pr for pr in psutil.process_iter() if (pr.name() == "jupyter-noteboo" and pr.username() == username)]
+
+
+def getPort(pr):
+    port = [el.laddr[1] for el in psutil.net_connections() if el.pid == pr.pid][0]
+    return port
+
+
+def killNotebook(pr, port=None):
+    if port:
+        if getPort(pr) == int(port):
+            pr.kill()
+    else:
+        pr.kill()
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        if not isInputPort(sys.argv[1]):
+            print("Invalid port format", file=sys.stderr)
+            sys.exit(1)
+        for pr in getNotebooks():
+            killNotebook(pr, sys.argv[1])
+    else:
+        for pr in getNotebooks():
+            killNotebook(pr)
---
+++ sshRelated/killPortProcess.py
@@ -0,0 +1,17 @@
+#!/lscr_paper/allan/miniconda3/envs/msth/bin/python
+
+import sys
+import psutil
+
+
+if len(sys.argv) == 1 or not sys.argv[1].isdigit():
+    print("Input error! Input not provided or it isn't a integer (port number)", file=sys.stderr)
+    sys.exit(1)
+
+port = int(sys.argv[1])
+
+for nc in psutil.net_connections():
+    if nc.laddr[1] == port:
+        for pr in psutil.process_iter():
+            if pr.pid == nc.pid:
+                pr.kill()
