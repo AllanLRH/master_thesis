8fd3633107107f12d63c250971511de83138c990
Allan L. R. Hansen <allan.lrh@gmail.com>
2016-12-01 14:42:47 +0100

    Updated nanEqual function in speclib/misc.py

--- speclib/misc.py
+++ speclib/misc.py
@@ -1,1 +1,6 @@
-nanEqual = lambda a, b: np.all((a == b) | (np.isnan(a) & np.isnan(b))) 
+def nanEqual(a, b):
+    if not (hasattr(a, 'shape') or hasattr(b, 'shape')):
+        raise ValueError("Inputs a nad b must both have the shape-attribute (like numpy arrays)")
+    if a.shape != b.shape:
+        raise ValueError("Inputs shapes must be identical. a.shape = {} and b.shape is {}".format(a.shape, b.shape)
+    return np.all( (a == b) | (np.isnan(a) & np.isnan(b)) ) 
