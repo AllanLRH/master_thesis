e9416e04497f2f1dac092a5705eee6e02d95fc70
Allan L. R. Hansen <allan.lrh@gmail.com>
2016-12-16 18:10:34 +0100

    Added barSBS to speclib/plotting

--- speclib/plotting.py
+++ speclib/plotting.py
@@ -1,6 +1,9 @@
-#!/usr/bin/env python
+#!/usr/bin/env ipython
 # -*- coding: utf8 -*-
 from collections import Iterable
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
 
 
 def looseAxesLimits(ax, loosen=0.05):
@@ -52,3 +55,42 @@ def looseAxesLimits(ax, loosen=0.05):
     ax.set_ylim(newYLinm)
 
 
+def barSBS(ax, *args, offset=0.04, extraGroupSpace=None):
+    """Plots dataseries as a bar chart, with the series bars next to each other.
+    The passed order is preserved.
+
+    Args:
+        ax (axes): Axes to plot on.
+        *args (dict): Data series contained in a dict. 'y' and 'label' are required keys, referenceing to data and label respectively. 'x' is x-values and is optional.
+        offset (float, optional): Space between individual bars, default 0.04.
+        extraGroupSpace (float, optional): Extra space between groups, default is 0 for less than 4 groups.
+
+    Returns:
+        TYPE: Description
+    """
+    dataDicts = args
+    if extraGroupSpace is None:
+        extraGroupSpace = 0.0 if len(dataDicts) <= 3 else 0.015 * len(dataDicts)
+    getNextColor = lambda: next(ax._get_lines.prop_cycler)['color']
+    width = 1/(1.35*len(dataDicts)) - offset/2 - extraGroupSpace/2
+    for i, dct in enumerate(dataDicts):
+        if 'x' in dct:
+            x = dct['x']
+        else:
+            x = np.arange(len(dct['y']))
+        ax.bar(x+extraGroupSpace+i*(width + offset), dct['y'], width, color=getNextColor(), label=dct['label'])
+    xtickLabels = ["%d" % el for el in ax.get_xticks()]
+    ax.set_xticks(ax.get_xticks() + 0.35)
+    ax.set_xticklabels(xtickLabels)
+    ax.legend()
+
+
+if __name__ == '__main__':
+    d0 = {'y': (5 + np.random.randn(24))**2, 'label': 'SMS'}
+    d1 = {'y': (5 + np.random.randn(24))**2, 'label': 'Call'}
+    d2 = {'y': (5 + np.random.randn(24))**2, 'label': 'Call2'}
+    d3 = {'y': (5 + np.random.randn(24))**2, 'label': 'SMS2'}
+    fig, (ax0, ax1) = plt.subplots(ncols=1, nrows=2, figsize=(16, 6))
+    barSBS(ax0, d0, d1)
+    barSBS(ax1, d0, d1, d2, d3)
+    plt.show()
