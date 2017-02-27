f48e6a3b43d02f9154b444a1391aa9bb45e35a20
Allan L. R. Hansen <allan.lrh@gmail.com>
2017-02-27 19:29:18 +0100

    Added the functions _userDF2communicationDictOfDicts, userDF2nxGraph, userDF2activityDataframe to speclig/graph.py

--- speclib/graph.py
+++ speclib/graph.py
@@ -3,6 +3,7 @@
 
 import numpy as np
 import networkx as nx
+import pandas as pd
 import igraph as ig
 
 
@@ -37,3 +38,77 @@ def igraph2networkx(igGraph):
             igGraph.get_adjacency().data
             )
         )  # noqa
+
+
+def _userDF2communicationDictOfDicts(df, userColumn='user',
+                                     associatedUserColumn='contactedUser'):
+    """
+    Turn a DataFrame with user data into a dict of dicts, which is easily converted to a
+    Networkx Graph or and adjacency-matrix-like DataFrame.
+
+    Args:
+        df (DataFrame): DataFrame as the one loaded by loadUsersParallel.
+        userColumn (str, optional): Name of the level in the index containing,
+                                    users initiating the communication.
+        associatedUserColumn (str, optional): Name of the column containing users
+                                              communicated to or associated with.
+
+    Returns:
+        Dict: Dict of dicts, listing connections and the numbers of events from outer
+              key (user) to inner key (user).
+    """
+    userIndex = np.sort(df.index.get_level_values(userColumn).unique())
+    communicationDct = dict()
+    for userInit in userIndex:
+        comCount = df.loc[userInit][associatedUserColumn].value_counts()
+        communicationDct[userInit] = comCount.to_dict()
+    return communicationDct
+
+
+def userDF2nxGraph(df, userColumn='user', associatedUserColumn='contactedUser',
+                   comtype=None, diGraph=False):
+    """Convert user DataFrame to a Networkx Graph.
+
+    Args:
+    df (DataFrame): DataFrame as the one loaded by loadUsersParallel.
+    userColumn (str, optional): Name of the level in the index containing,
+                                users initiating the communication.
+    associatedUserColumn (str, optional): Name of the column containing users
+                                          communicated to or associated with.
+    comtype (str, optional): Filter communication type.
+    diGraph (bool, optional): Set to True to return a DiGraph rather thatn a Graph.
+
+    Returns:
+        nx.Graph: A Networkx graph.
+    """
+    if comtype is not None:
+        df = df[df.index.get_level_values('comtype') == comtype]
+    communicationDct = _userDF2communicationDictOfDicts(df, userColumn=userColumn)
+    if diGraph:
+        return nx.DiGraph(communicationDct)
+    # Drop the weights: Only the keys (users communicated to) is necessary
+    return nx.Graph({k: v.keys() for (k, v) in communicationDct.items()})
+
+
+def userDF2activityDataframe(df, userColumn='user', associatedUserColumn='contactedUser',
+                             comtype=None):
+    """Create an adjacency-matrix like DataFrame from the regular communication DataFrame.
+
+    Args:
+    df (DataFrame): DataFrame as the one loaded by loadUsersParallel.
+    userColumn (str, optional): Name of the level in the index containing,
+                                users initiating the communication.
+    associatedUserColumn (str, optional): Name of the column containing users
+                                          communicated to or associated with.
+    comtype (str, optional): Filter communication type.
+
+    Returns:
+        DataFrame: Adjacency-matrix like DataFrame
+    """
+    if comtype is not None:
+        df = df[df.index.get_level_values('comtype') == comtype]
+    communicationDct = _userDF2communicationDictOfDicts(df, userColumn=userColumn)
+    activityDf = pd.DataFrame(communicationDct)
+    activityDf.index.name = 'userInit'
+    activityDf.columns.name = 'userRecv'
+    return activityDf
