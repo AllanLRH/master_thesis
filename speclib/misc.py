f0ef31657f975e461306d24f62c7f96def8f750e
Allan L. R. Hansen <allan.lrh@gmail.com>
2017-03-28 14:08:46 +0200

    Updated docstring formatting

--- speclib/misc.py
+++ speclib/misc.py
@@ -21,7 +21,8 @@ def nanEqual(a, b):
     if not (hasattr(a, 'shape') or hasattr(b, 'shape')):
         raise ValueError("Inputs a nad b must both have the shape-attribute (like numpy arrays)")
     if a.shape != b.shape:
-        raise ValueError("Inputs shapes must be identical. a.shape = {} and b.shape is {}".format(a.shape, b.shape))
+        raise ValueError("Inputs shapes must be identical. a.shape = {} and b.shape is {}"
+                         .format(a.shape, b.shape))
     return np.all( (a == b) | (np.isnan(a) & np.isnan(b)) )  # noqa
 
 
