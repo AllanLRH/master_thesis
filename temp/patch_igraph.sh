e6ed26550fd89bd66674735399e9f529baf64328
Allan L. R. Hansen <allan.lrh@gmail.com>
2017-05-09 16:10:42 +0200

    Added second igraph patch, forcing ipython notebook to render PNG rather than SVG

--- temp/patch_igraph.sh
+++ temp/patch_igraph.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env sh
+# From https://github.com/igraph/python-igraph/commit/8864b46849b031a3013764d03e167222963c0f5d
 patch $(python -c "import igraph; print(igraph.drawing.__file__)") ./igraph_svg_draw.patch
+# From http://stackoverflow.com/questions/30640489/issue-plotting-vertex-labels-using-igraph-in-ipython
+patch $(python -c "import igraph; print(igraph.drawing.__file__)") ./igraph_svg_to_png_draw.patch
