a393001eb21aff811c010a4bcd1856a38dd28d7f
Allan L. R. Hansen <allan.lrh@gmail.com>
2018-01-24 16:27:58 +0100

    Prepared for multiprocessing, fails due to an itetable being None.

--- morequestions/bluetooth_adjacent_one_user_at_a_time.py
+++ morequestions/bluetooth_adjacent_one_user_at_a_time.py
@@ -8,24 +8,34 @@ import numpy as np  # noqa
 import pandas as pd
 import itertools
 from speclib import loaders
+from multiprocessing import Pool
 
-
-ua = loaders.Useralias()
-userlist = loaders.getUserList()
+pd.set_option('display.max_rows', 55)
+pd.set_option('display.max_columns', 10)
+pd.set_option('display.width', 1000)
 
 # # Goal
 #
 # * Filter data for omnipresent devices, which are a bad proxy for social interactions
 # * Group data
 #
-user = userlist[0]
-morning_hour = 7
-evening_hour = 17
+
+
+
+def concatenater(args, frac=0.70):
+    """
+    Concatenates users in timebin, present `frac` part of the time
+    """
+    vc = args.value_counts()
+    return set(vc[vc >= frac*vc.max()].index)
+
+
+def mostly_present_counter(args):
+    return len(concatenater(args))
 
 
 def filterUserMac(*args):
-    user, ua, evening_hour, morning_hour = args
-    df = loaders.loadUserBluetooth(user, ua)
+    df, user, ua, evening_hour, morning_hour = args
     if df is None:
         return None
     # Filter out time where they're probably at DTU
@@ -45,7 +55,48 @@ def filterUserMac(*args):
     return cnt, var
 
 
-res = dict()
-for user in userlist:
-    print("Processing user", ua[user])
-    res[user] = filterUserMac(user, ua.userdct, evening_hour, morning_hour)
+def main(user):
+    # ****************************************************************************
+    # *                         Initial filtering of data                        *
+    # ****************************************************************************
+    ua = loaders.Useralias()  # noqa
+    morning_hour = 7
+    evening_hour = 17
+    df = loaders.loadUserBluetooth(user, ua)
+    cnt, var = filterUserMac(df, user, ua.userdct, evening_hour, morning_hour)
+    remove_from_index = set(cnt[cnt > 30].index)
+    df = df[~df.bt_mac.isin(remove_from_index)]
+
+    # ****************************************************************************
+    # *           Filter data to contain only free time before workdays          *
+    # ****************************************************************************
+    df['hour'] = df.index.hour  # noqa
+    print("Done computing hour")
+    df['weekday'] = df.index.weekday
+    print("Done computing weekday")
+    before_workday = df.weekday.isin({0, 1, 2, 3, 6})  # is it monday, tuesday, wendnesday, thursday or sunday?
+    print("Done computing before_workday")
+    free_time = (19 < df.hour) | (df.hour < 7)
+    print("Done computing free_time")
+    dfs = df[before_workday & free_time]
+
+    dfs['scanned_user'] = dfs.scanned_user.replace(np.NaN, df.bt_mac)
+
+    grouped = dfs.iloc[:3000].groupby('user')[['scanned_user']].resample('90T', closed='left').agg(concatenater)
+    grouped['scanned_user'] = grouped.scanned_user.replace(set(), np.NaN)
+
+    print("Fraction of non-nulls:", grouped.scanned_user.notnull().sum() / grouped.shape[0])
+    print("Number of of non-nulls:", grouped.scanned_user.notnull().sum())
+    return grouped
+
+
+if __name__ == '__main__':
+    userlist = loaders.getUserList()
+    try:
+        pool = Pool(16)
+        res = pool.map(main, userlist)
+        grouped_res = {userlist[i]: res[i] for i in range(len(userlist))}
+    except Exception as err:
+        raise(err)
+    finally:
+        pool.close()
