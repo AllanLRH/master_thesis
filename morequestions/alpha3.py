3a0c7c659a7ee03937c8a2fe5a78ed546e183b37
Allan L. R. Hansen <allan.lrh@gmail.com>
2018-05-07 17:11:12 +0200

    Added function to compute s**2, t**2 and r

--- morequestions/alpha3.py
+++ morequestions/alpha3.py
@@ -124,7 +124,20 @@ def get_q_mean(g, q):
     return q_mean
 
 
-
+def get_s2_t2_r(g, q, q_mean):
+    numerator_s   = 0.0
+    numerator_t   = 0.0
+    denominator = 0.0
+    for i in g.nodes:
+        for j in g[i]:  # w_ij should be treated as 0 when no connection exists
+            if i != j:
+                numerator_s += g[i][j]['w_ij'] * ((q.loc[i] - q_mean)**2 + (q.loc[j] - q_mean)**2)
+                numerator_t += g[i][j]['w_ij'] * ((q.loc[i] - q_mean) * (q.loc[j] - q_mean))
+                denominator += 2*g[i][j]['w_ij']
+    s_squared = numerator_s / denominator
+    t_squared = numerator_t / denominator
+    r = numerator_t / numerator_s
+    return s_squared, t_squared, r
 
 
 
