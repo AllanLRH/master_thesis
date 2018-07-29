3f2fc6ab41de7634b3c040db896b3c922f94316c
Allan L. R. Hansen <allan.lrh@gmail.com>
2018-07-29 20:33:51 +0200

    Added logging to pushbulletnotifier

--- speclib/pushbulletNotifier.py
+++ speclib/pushbulletNotifier.py
@@ -5,6 +5,15 @@ from pushbullet import Pushbullet
 import datetime
 import inspect
 import traceback
+import logging
+import os
+
+dirname   = os.path.dirname(__file__)
+logfolder = os.path.abspath(os.path.join(dirname, '../logs/'))
+logging.basicConfig(filename=os.path.join(logfolder, 'pushbulletnotifier.log'),
+                    level=logging.INFO,
+                    format="%(asctime)s :: %(filename)s:%(lineno)s :: %(funcName)s() ::    %(message)s")
+logger = logging.getLogger('pushbulletnotifier')
 
 phoneName = "OnePlus ONEPLUS A6003"
 
@@ -16,6 +25,7 @@ class JobNotification(object):
 
     def __init__(self, devices="phone"):
         super(JobNotification, self).__init__()
+        logger.info(f"Created JobNotification instance for devices = {devices}")
         self.startTime = datetime.datetime.now()
         self.timeFormatString = r'%d/%m/%Y %H:%M:%S'
         if devices == "all":
@@ -26,21 +36,27 @@ class JobNotification(object):
                     self.pb = dv
 
     def pretty_time_delta(self, td):
-        seconds = int(td.total_seconds())
-        days, seconds = divmod(seconds, 86400)
-        hours, seconds = divmod(seconds, 3600)
+        seconds          = int(td.total_seconds())
+        days, seconds    = divmod(seconds, 86400)
+        hours, seconds   = divmod(seconds, 3600)
         minutes, seconds = divmod(seconds, 60)
+        logger.debug(f"seconds = {seconds}, minutes = {minutes}, hours = {hours}, days = {days}")
         if days > 0:
-            return '%dd%dh%dm%ds' % (days, hours, minutes, seconds)
+            formatted_timedelta = '%dd%dh%dm%ds' % (days, hours, minutes, seconds)
         elif hours > 0:
-            return '%dh%dm%ds' % (hours, minutes, seconds)
+            formatted_timedelta = '%dh%dm%ds' % (hours, minutes, seconds)
         elif minutes > 0:
-            return '%dm%ds' % (minutes, seconds)
+            formatted_timedelta = '%dm%ds' % (minutes, seconds)
         else:
-            return '%ds' % (seconds,)
+            formatted_timedelta = '%ds' % (seconds,)
+        logging.debug(f"formatted_timedelta = {formatted_timedelta}")
+        return formatted_timedelta
 
     def send(self, exception=None, message=None):
-        self.endTime = datetime.datetime.now()
+        logger.info(f"Entered send, type(exception) = {type(exception)}, type(message) = {type(message)}")
+        logger.debug(f"message = {message}")
+        logger.debug(f"exception = {repr(exception)}")
+        self.endTime   = datetime.datetime.now()
         callerFilename = inspect.stack()[1].filename
         if message is None:
             msgLst = list()
@@ -49,21 +65,26 @@ class JobNotification(object):
         msgLst = msgLst + ["Runtime: {timeDelta}",
                            "Started: {timeStarted}",
                            "Finished: {timeEnded}"]
+        logger.debug(f"msgLst = {msgLst}")
         if exception is not None:
-            title = f"Error! {callerFilename} have thrown an exception!"
-            traceback_string = "".join(traceback.format_exc())
+            title                = f"Error! {callerFilename} have thrown an exception!"
+            logger.debug(f"title = {title}")
+            traceback_string     = "".join(traceback.format_exc())
+            logger.debug(f"traceback_string = {traceback_string}")
             visual_division_line = '* '*len(str(exception))
-            msgLst = [str(exception).title(),
-                      visual_division_line,
-                      traceback_string,
-                      visual_division_line] + msgLst
+            logger.debug(f"visual_division_line = {visual_division_line}")
+            msgLst               = [str(exception).title(),
+                                    visual_division_line,
+                                    traceback_string,
+                                    visual_division_line] + msgLst
+            logger.debug(f"msgLst (there was an exception) = {msgLst}")
         else:
             title = f"{callerFilename} is done"
         msg = "\n".join(msgLst).format(
-            callerFilename=callerFilename,
-            timeDelta=self.pretty_time_delta(self.endTime - self.startTime),
-            timeStarted=self.startTime.strftime(self.timeFormatString),
-            timeEnded=self.endTime.strftime(self.timeFormatString))
+            callerFilename = callerFilename,  # noqa
+            timeDelta      = self.pretty_time_delta(self.endTime - self.startTime),  # noqa
+            timeStarted    = self.startTime.strftime(self.timeFormatString),  # noqa
+            timeEnded      = self.endTime.strftime(self.timeFormatString))  # noqa
         self.pb.push_note(title, msg)
 
 
