c08afdf54e4bed4da4a25db8b7ab9725eb8b2c84
Allan L. R. Hansen <allan.lrh@gmail.com>
2018-07-31 21:08:28 +0200

    Updated subgrid sgd search, should run fast with around 224 iterations.

--- userMovement/userMovement_cv_subgrid_search_sgd.py
+++ userMovement/userMovement_cv_subgrid_search_sgd.py
@@ -4,6 +4,7 @@ sys.path.append(os.path.abspath(".."))
 import matplotlib as mpl
 mpl.use('agg')
 import matplotlib.pyplot as plt
+import pickle
 import seaborn as sns
 sns.set(context='paper', style='whitegrid', color_codes=True, font_scale=1.8)
 colorcycle = [(0.498, 0.788, 0.498),
@@ -25,7 +26,7 @@ warnings.simplefilter(action='ignore', category=FutureWarning)
 import numpy as np
 from time import sleep
 
-from speclib import pushbulletNotifier, plotting
+from speclib import pushbulletNotifier, plotting, misc
 
 np.set_printoptions(linewidth=145)
 
@@ -36,11 +37,12 @@ from sklearn import metrics, model_selection, preprocessing
 from sklearn.decomposition import PCA
 from sklearn.pipeline import Pipeline
 
+misc.wait_for_cpu_resources(check_interval=15)
 
 import logging
-logging.basicConfig(filename='userMovement_sgd.log', level=logging.INFO,
+logging.basicConfig(filename='userMovement_sgd_subgrid search.log', level=logging.INFO,
                     format="%(asctime)s :: %(filename)s:%(lineno)s :: %(funcName)s() ::    %(message)s")
-logger = logging.getLogger('userMovement_sgd')
+logger = logging.getLogger('userMovement_sgd_sgs')
 
 data_path = '../../allan_data/DataPredictMovement_half.p'
 x, y = np.load(data_path)
@@ -48,20 +50,18 @@ x = x.astype(float)
 logger.info(f"Loaded data")
 
 jn = pushbulletNotifier.JobNotification(devices="phone")
-
+jn.send(message="Started fine crossvalidation search.")
 
 processes = 24
 try:
     x_re, x_va, y_re, y_va = model_selection.train_test_split(x, y, test_size=0.2, stratify=y)
     logger.info(f"Split data in to training set and validation set.")
-    pipe = Pipeline([('scaler', preprocessing.StandardScaler()), ('pca', PCA()), ('sgd', SGDClassifier())])
+    pipe = Pipeline([('pca', PCA()), ('scaler', preprocessing.StandardScaler()),
+                     ('sgd', SGDClassifier(class_weight='balanced', loss='log', penalty='l2'))])
     subsearch_param_grid = {
-        'pca__n_components': np.array([29, 30, 31]),
-        'sgd__penalty': ['l2'],
-        'sgd__alpha': np.linspace(0.5, 2, 20),
-        'sgd__loss': ['modified_huber']
-        }  # noqa
-    subsearch_param_grid
+        'pca__n_components': np.arange(25, 33),
+        'sgd__alpha': 10**np.linspace(-1, 2, 8),
+        }   # noqa
     logger.info(f"Starting cross validation")
     est = model_selection.GridSearchCV(pipe, subsearch_param_grid, scoring='roc_auc', cv=4, verbose=49, refit=True,
                                        n_jobs=processes, pre_dispatch=processes, return_train_score=True)
@@ -78,6 +78,14 @@ try:
     logger.info(f"AUC score for validation set of size {len(y_va)} is {validation_auc_score:.5f}")
     fig, ax, aucscore = plotting.plotROC(y_va, yhat)
     fig.savefig('figs/userMovement_cv_subgrid_search_roc_curve.pdf')
+    est.y_va = y_va  # save for plotting ROC curve later
+    est.yhat = yhat  # save for plotting ROC curve later
+    est.validation_auc_score = validation_auc_score
+    est.x_va = x_va
+    est.y_va = y_va
+    with open("userMovement_cv_subgrid_search.pkl", 'bw') as fid:
+        pickle.dump(est, fid)
+
 except Exception as err:
     jn.send(err)
     sleep(8)
