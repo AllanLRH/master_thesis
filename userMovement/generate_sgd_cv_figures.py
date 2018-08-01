cf08712602a8a5b49042684db8c3050815785d09
Allan L. R. Hansen <allan.lrh@gmail.com>
2018-08-01 19:47:59 +0200

    Removed try.except block and added code for generating LaTeX tables, exported to JSON

--- userMovement/generate_sgd_cv_figures.py
+++ userMovement/generate_sgd_cv_figures.py
@@ -37,48 +37,61 @@ sns.set_palette(colorcycle)
 mpl.rcParams['figure.max_open_warning'] = 65
 mpl.rcParams['figure.figsize'] = np.array([12, 7]) * 0.75
 mpl.rcParams['text.usetex'] = True
+import json
+
 
 with open('userMovement_sgd_std_final_coarse.pkl', 'br') as fid:
     est = pickle.load(fid)
 cvr = pd.DataFrame(est.cv_results_)
 
+description_file = "figs/crossval_pca_alpha_descriptions.json"
+description_dict = dict()
+
 for weight, penalty, loss in itertools.product(cvr.param_sgd__class_weight.unique(), cvr.param_sgd__penalty.unique(), cvr.param_sgd__loss.unique()):
     print(f'Processing combination {weight}, {penalty}, {loss}')
-    try:
-        if weight is None:
-            mask = cvr.param_sgd__class_weight.isna() & (cvr.param_sgd__penalty == penalty) & (cvr.param_sgd__loss == loss)
-        else:
-            mask = (cvr.param_sgd__class_weight == weight) & (cvr.param_sgd__penalty == penalty) & (cvr.param_sgd__loss == loss)
-        name = f'weight_{weight}_penalty_{penalty}_loss_{loss}'
-        to_plot_mean = cvr[mask].groupby(['param_pca__n_components', 'param_sgd__alpha']).mean_test_score.mean().T.unstack()
-        to_plot_std = cvr[mask].groupby(['param_pca__n_components', 'param_sgd__alpha']).std_test_score.mean().T.unstack()
-        to_plot_mean.columns.name = 'alpha'
-        to_plot_std.columns.name = 'alpha'
-        to_plot_mean.index.name = 'PCA components'
-        to_plot_std.index.name = 'PCA components'
-        xticklabels = ["$10^{%.2f}$" % el for el in np.log10(to_plot_mean.columns)]
-        xticklabels = ["$10^{%.2f}$" % el for el in np.log10(to_plot_std.columns)]
-        yticklabels = ["${}$".format(el) for el in to_plot_mean.index]
-        yticklabels = ["${}$".format(el) for el in to_plot_std.index]
-        with sns.axes_style('ticks'):
-            fig, ax = plt.subplots(figsize=[12, 7])
-            sns.heatmap(to_plot_mean, ax=ax, cmap='inferno')
-            ax.set_xlabel(r'$\alpha$')
-            ax.set_ylabel('PCA components')
-            ax.set_xticklabels(xticklabels, rotation='vertical')
-            ax.set_yticklabels(yticklabels)
-            plt.tight_layout()
-            fig.savefig(f'figs/crossval_pca_alpha_rough_{name}_mean_score.pdf')
-        with sns.axes_style('ticks'):
-            fig, ax = plt.subplots(figsize=[12, 7])
-            sns.heatmap(to_plot_std, ax=ax, cmap='inferno')
-            ax.set_xlabel(r'$\alpha$')
-            ax.set_ylabel('PCA components')
-            ax.set_xticklabels(xticklabels, rotation='vertical')
-            ax.set_yticklabels(yticklabels)
-            plt.tight_layout()
-            fig.savefig(f'figs/crossval_pca_alpha_rough_{name}_std_score.pdf')
-    except ValueError as err:
-        print("Failed!")
-        print(err)
-        print("Continiuing!")
+    if weight is None:
+        mask = cvr.param_sgd__class_weight.isna() & (cvr.param_sgd__penalty == penalty) & (cvr.param_sgd__loss == loss)
+    else:
+        mask = (cvr.param_sgd__class_weight == weight) & (cvr.param_sgd__penalty == penalty) & (cvr.param_sgd__loss == loss)
+    name = f'weight_{weight}_penalty_{penalty}_loss_{loss}'
+
+    scores = cvr[mask][['mean_train_score', 'mean_test_score', 'std_train_score', 'std_test_score']]
+    scores.rename(columns={'mean_train_score': 'Mean training score',
+                           'mean_test_score': 'Mean testing score',
+                           'std_test_score': 'Std testing score',
+                           'std_train_score': 'Std training score'}, inplace=True)
+    # description_dict[name] = scores.describe().loc[['mean', 'std', 'min', '25%', '50%', '75%', 'max']].to_latex()
+    description_dict[name] = scores.describe().to_latex()
+
+    to_plot_mean = cvr[mask].groupby(['param_pca__n_components', 'param_sgd__alpha']).mean_test_score.mean().T.unstack()
+    to_plot_std = cvr[mask].groupby(['param_pca__n_components', 'param_sgd__alpha']).std_test_score.mean().T.unstack()
+    to_plot_mean.columns.name = 'alpha'
+    to_plot_std.columns.name = 'alpha'
+    to_plot_mean.index.name = 'PCA components'
+    to_plot_std.index.name = 'PCA components'
+    xticklabels = ["$10^{%.2f}$" % el for el in to_plot_mean.columns]
+    xticklabels = ["$10^{%.2f}$" % el for el in to_plot_std.columns]
+    yticklabels = ["${}$".format(el) for el in to_plot_mean.index]
+    yticklabels = ["${}$".format(el) for el in to_plot_std.index]
+    with sns.axes_style('ticks'):
+        fig, ax = plt.subplots(figsize=[12, 7])
+        sns.heatmap(to_plot_mean, ax=ax, cmap='inferno', vmin=0.74, vmax=0.86)
+        ax.set_xlabel(r'$\alpha$')
+        ax.set_ylabel('PCA components')
+        ax.set_xticklabels(xticklabels, rotation='vertical')
+        ax.set_yticklabels(yticklabels)
+        plt.tight_layout()
+        fig.savefig(f'figs/crossval_pca_alpha_rough_{name}_mean_score.pdf')
+    with sns.axes_style('ticks'):
+        fig, ax = plt.subplots(figsize=[12, 7])
+        sns.heatmap(to_plot_std, ax=ax, cmap='inferno', vmin=0.0, vmax=0.0015)
+        ax.set_xlabel(r'$\alpha$')
+        ax.set_ylabel('PCA components')
+        ax.set_xticklabels(xticklabels, rotation='vertical')
+        ax.set_yticklabels(yticklabels)
+        plt.tight_layout()
+        fig.savefig(f'figs/crossval_pca_alpha_rough_{name}_std_score.pdf')
+
+with open(description_file, 'w') as fid:
+    json.dump(description_dict, fid, indent=4)
+
