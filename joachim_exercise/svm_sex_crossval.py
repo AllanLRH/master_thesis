ed983a740988cfd6e9c2aa1ec11812a246c3fc36
Allan L. R. Hansen <allan.lrh@gmail.com>
2018-11-17 00:26:03 +0100

    Add AUC evaluation for entire dataset

--- joachim_exercise/svm_sex_crossval.py
+++ joachim_exercise/svm_sex_crossval.py
@@ -46,10 +46,10 @@ cv_args = dict(scoring = 'roc_auc',        # noqa
                # pre_dispatch = 2 * n_jobs,  # noqa
                return_train_score = True)  # noqa
 
-svc_kwargs = dict(probability = True,          # noqa
-                  class_weight = 'balanced')  # noqa
+svc_kwargs = dict(probability = True)  # noqa
 
-svc_param_space_shared = {'svc__C': 2.0**np.linspace(-10, 10, 11)}
+svc_param_space_shared = {'svc__C': 2.0**np.linspace(-12, 10, 23),
+                          'svc__class_weight': ['balanced', None]}
 
 # ****************************************************************************
 # *                                 Load data                                *
@@ -75,16 +75,20 @@ try:
     param_grid_lin = dict()
     param_grid_lin.update(svc_param_space_shared)
 
-    est_lin      = model_selection.GridSearchCV(pipe_lin, param_grid_lin, **cv_args)
+    est_lin            = model_selection.GridSearchCV(pipe_lin, param_grid_lin, **cv_args)
     est_lin.fit(X_tr, y_tr)
-    _, prob1_lin = est_lin.best_estimator_.predict_proba(X_va).T
-    AUC_lin      = metrics.roc_auc_score(y_va, prob1_lin)
+    _, prob1_lin       = est_lin.best_estimator_.predict_proba(X_va).T
+    _, prob1_lin_full  = est_lin.best_estimator_.predict_proba(X).T
+    AUC_lin            = metrics.roc_auc_score(y_va, prob1_lin_full)
 
-    est_lin.best_auc_  = AUC_lin
-    est_lin.X_tr       = X_tr
-    est_lin.y_tr       = y_tr
-    est_lin.X_va       = X_va
-    est_lin.y_va       = y_va
+    AUC_lin_full  = metrics.roc_auc_score(y, )
+
+    est_lin.best_auc_       = AUC_lin
+    est_lin.best_auc_full_  = AUC_lin_full
+    est_lin.X_tr_           = X_tr
+    est_lin.y_tr_           = y_tr
+    est_lin.X_va_           = X_va
+    est_lin.y_va_           = y_va
     with open("gender_prediction_svm_lin_kernel_auc_score.pkl", "wb") as fid:
         pickle.dump(est_lin, fid)
 except Exception as err:
@@ -105,16 +109,20 @@ try:
     param_grid_poly = {'svc__degree': [2, 3, 4, 5]}
     param_grid_poly.update(svc_param_space_shared)
 
-    est_poly      = model_selection.GridSearchCV(pipe_poly, param_grid_poly, **cv_args)
+    est_poly           = model_selection.GridSearchCV(pipe_poly, param_grid_poly, **cv_args)
     est_poly.fit(X_tr, y_tr)
-    _, prob1_poly = est_poly.best_estimator_.predict_proba(X_va).T
-    AUC_poly      = metrics.roc_auc_score(y_va, prob1_poly)
+    _, prob1_poly      = est_poly.best_estimator_.predict_proba(X_va).T
+    _, prob1_poly_full = est_poly.best_estimator_.predict_proba(X).T
+    AUC_poly           = metrics.roc_auc_score(y_va, prob1_poly_full)
 
-    est_poly.best_auc_ = AUC_poly
-    est_poly.X_tr      = X_tr
-    est_poly.y_tr      = y_tr
-    est_poly.X_va      = X_va
-    est_poly.y_va      = y_va
+    AUC_poly_full = metrics.roc_auc_score(y, )
+
+    est_poly.best_auc_      = AUC_poly
+    est_poly.best_aucfull__ = AUC_poly_full
+    est_poly.X_tr_          = X_tr
+    est_poly.y_tr_          = y_tr
+    est_poly.X_va_          = X_va
+    est_poly.y_va_          = y_va
     with open("gender_prediction_svm_poly_kernel_auc_score.pkl", "wb") as fid:
         pickle.dump(est_poly, fid)
 except Exception as err:
@@ -135,16 +143,20 @@ try:
     param_grid_rbf = {'svc__gamma': 2.0**np.linspace(-10, 2, 13)}
     param_grid_rbf.update(svc_param_space_shared)
 
-    est_rbf      = model_selection.GridSearchCV(pipe_rbf, param_grid_rbf, **cv_args)
+    est_rbf            = model_selection.GridSearchCV(pipe_rbf, param_grid_rbf, **cv_args)
     est_rbf.fit(X_tr, y_tr)
-    _, prob1_rbf = est_rbf.best_estimator_.predict_proba(X_va).T
-    AUC_rbf      = metrics.roc_auc_score(y_va, prob1_rbf)
+    _, prob1_rbf       = est_rbf.best_estimator_.predict_proba(X_va).T
+    _, prob1_rbf_full  = est_rbf.best_estimator_.predict_proba(X).T
+    AUC_rbf            = metrics.roc_auc_score(y_va, prob1_rbf_full)
 
-    est_rbf.best_auc_  = AUC_rbf
-    est_rbf.X_tr       = X_tr
-    est_rbf.y_tr       = y_tr
-    est_rbf.X_va       = X_va
-    est_rbf.y_va       = y_va
+    AUC_rbf_full  = metrics.roc_auc_score(y, )
+
+    est_rbf.best_auc_       = AUC_rbf
+    est_rbf.best_auc_full_  = AUC_rbf_full
+    est_rbf.X_tr_           = X_tr
+    est_rbf.y_tr_           = y_tr
+    est_rbf.X_va_           = X_va
+    est_rbf.y_va_           = y_va
     with open("gender_prediction_svm_rbf_kernel_auc_score.pkl", "wb") as fid:
         pickle.dump(est_rbf, fid)
 except Exception as err:
